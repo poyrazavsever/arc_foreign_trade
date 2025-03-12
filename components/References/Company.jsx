a34d68f78795a10000a4467177d6e7eef1463d1f
Poyraz Avsever <poyrazavsever@hotmail.com>
2025-03-12 11:49:15 +0300

    feat: updated companies list

--- components/References/Company.jsx
+++ components/References/Company.jsx
@@ -12,12 +12,12 @@ const Company = () => {
       servicesDescription: "ARC Danışmanlık olarak, Şah Radyatör'ün uluslararası pazarda güçlü bir konum elde etmesi için kapsamlı dış ticaret ve pazarlama çözümleri sunduk. Hedef pazar analizleri ile firmanın doğru müşteri kitlesine ulaşmasını sağlarken, dijital pazarlama ve marka konumlandırma stratejileri ile global bilinirliğini artırdık. Kültürel ve dilsel uyumluluk danışmanlığımız sayesinde, farklı ülkelerdeki müşteri beklentilerine uygun etkili iletişim stratejileri geliştirdik. Şah Radyatör'ün ihracat operasyonlarını optimize ederek, yurtdışı satışlarını artırmasına ve uluslararası rekabette avantaj kazanmasına katkıda bulunduk."
     },
     {
-      logoSrc: "/Logos/1.png",
-      alt: "EKSFORM Logo",
-      title: "EKSFORM",
-      description: "Form Plastik- Adnan E. Bölükoğlu ve Onur Kalıp Sanayi- Hakan Ergen olarak Mayıs 2008 itibarıyle güçlerimizi EKS FORM Plastik Metal Kalıp Sanayi Ticaret Limited Şirketi olarak kurumsal bir çatı altında birleştirdik. Amacımız sizlerin isteklerini ileri teknolojiyle, yeni ürünler ve hizmetler içeren mükemmel çözümlerle karşılayarak büyümek, artan katma değerler kazandırmak.",
-      servicesTitle: "ARC Danışmanlık Olarak EKSFORM'a Sağladığımız Hizmetler",
-      servicesDescription: "ARC Danışmanlık olarak, EKSFORM'un endüstriyel üretim süreçlerini optimize etmek ve uluslararası pazarda etkin bir yer edinmesini sağlamak için stratejik danışmanlık sunduk. Pazar araştırmaları ve marka yönetimi konularında destek sağlayarak firmanın küresel ölçekte tanınırlığını artırmasına yardımcı olduk."
+      logoSrc: "/Logos/8.png",
+      alt: "Yetpar Logo",
+      title: "Yetpar",
+      description: "Yetpar Makina olarak, iş makinaları yedek parça tedariki ve üretiminde ulusal ve uluslararası arenada öncü bir marka olmak, yenilikçi çözümlerle sektöre yön vermek ve müşteri memnuniyetini en üst seviyeye çıkarmak. Sürdürülebilir iş modelleri ve yüksek kalite anlayışımızla sektörde güvenilirliği ve liderliği temsil etmeyi hedefliyoruz.",
+      servicesTitle: "ARC Danışmanlık Olarak Yetpar'a Sağladığımız Hizmetler",
+      servicesDescription: "ARC Danışmanlık olarak, Yetpar'ın küresel pazarlarda büyümesine destek verdik. Hedef pazar analizleri, dijital görünürlük artırma stratejileri ve uluslararası ticaret danışmanlığı ile firmanın sektördeki konumunu güçlendirdik."  
     },
     {
       logoSrc: "/Logos/2.png",
@@ -28,6 +28,14 @@ const Company = () => {
       servicesDescription: "ARC Danışmanlık olarak, İlhan Uluslararası Taşımacılık'ın küresel lojistik operasyonlarını güçlendirmek ve pazar genişlemesine destek olmak için dış ticaret ve stratejik yönetim hizmetleri sunduk. Uluslararası düzenlemelere uyum sağlayarak daha rekabetçi bir konuma ulaşmasına katkıda bulunduk."
     },
     {
+      logoSrc: "/Logos/1.png",
+      alt: "EKSFORM Logo",
+      title: "EKSFORM",
+      description: "Form Plastik- Adnan E. Bölükoğlu ve Onur Kalıp Sanayi- Hakan Ergen olarak Mayıs 2008 itibarıyle güçlerimizi EKS FORM Plastik Metal Kalıp Sanayi Ticaret Limited Şirketi olarak kurumsal bir çatı altında birleştirdik. Amacımız sizlerin isteklerini ileri teknolojiyle, yeni ürünler ve hizmetler içeren mükemmel çözümlerle karşılayarak büyümek, artan katma değerler kazandırmak.",
+      servicesTitle: "ARC Danışmanlık Olarak EKSFORM'a Sağladığımız Hizmetler",
+      servicesDescription: "ARC Danışmanlık olarak, EKSFORM'un endüstriyel üretim süreçlerini optimize etmek ve uluslararası pazarda etkin bir yer edinmesini sağlamak için stratejik danışmanlık sunduk. Pazar araştırmaları ve marka yönetimi konularında destek sağlayarak firmanın küresel ölçekte tanınırlığını artırmasına yardımcı olduk."
+    },
+    {
       logoSrc: "/Logos/3.png",
       alt: "Konar İnşaat Logo",
       title: "Konar İnşaat",
@@ -35,14 +43,6 @@ const Company = () => {
       servicesTitle: "ARC Danışmanlık Olarak Konar İnşaat'a Sağladığımız Hizmetler",
       servicesDescription: "ARC Danışmanlık olarak, Konar İnşaat'ın marka bilinirliğini artırarak uluslararası yatırımcılarla olan ilişkilerini geliştirdik. Pazar analizleri, kurumsal kimlik yönetimi ve dijital pazarlama stratejileri ile firmanın hedef kitleye ulaşmasını sağladık."
     },
-    {
-      logoSrc: "/Logos/8.png",
-      alt: "Yetpar Logo",
-      title: "Yetpar",
-      description: "Yetpar Makina olarak, iş makinaları yedek parça tedariki ve üretiminde ulusal ve uluslararası arenada öncü bir marka olmak, yenilikçi çözümlerle sektöre yön vermek ve müşteri memnuniyetini en üst seviyeye çıkarmak. Sürdürülebilir iş modelleri ve yüksek kalite anlayışımızla sektörde güvenilirliği ve liderliği temsil etmeyi hedefliyoruz.",
-      servicesTitle: "ARC Danışmanlık Olarak Yetpar'a Sağladığımız Hizmetler",
-      servicesDescription: "ARC Danışmanlık olarak, Yetpar'ın küresel pazarlarda büyümesine destek verdik. Hedef pazar analizleri, dijital görünürlük artırma stratejileri ve uluslararası ticaret danışmanlığı ile firmanın sektördeki konumunu güçlendirdik."  
-    }
   ];
 
   return (
