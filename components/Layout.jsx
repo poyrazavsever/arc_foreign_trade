0f7209f84beb5db2aa7f0b6fb249c5c3518f605b
poyrazavsever <poyrazavsever@hotmail.com>
2025-04-23 16:14:00 +0300

    feat: updated layout for wix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,22 +1,14 @@
 import React from 'react'
 import { Toaster, toast } from 'react-hot-toast'
 
-// Components
-import Navbar from './Navbar'
-import Footer from './Footer'
-
 const Layout = ({children}) => {
 
   return (
     <div>
-        
-        <Navbar />
 
         <main>
             {children}
         </main>
-
-        <Footer />
 
         <Toaster position="top-center" />
       
